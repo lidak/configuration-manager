39883fa4c3df286b033cccbd72a8b1a412cf401c
lidak <animatio.dev@gmail.com>
2018-04-04 17:19:30 +0300

    Config reader improved to get config path's from CL

--- modules/ConfigurationReader.js
+++ modules/ConfigurationReader.js
@@ -4,22 +4,21 @@ const pick = require('lodash.pick');
 const merge = require('lodash.merge');
 const cliArgs = require('cli-args');
 
-module.exports = class ConfigurationManager {
+module.exports = class ConfigurationReader {
   constructor({
-    configDir = './config',
-    configFilesExtension = 'yaml',
-    env = 'development'
+    baseConfigPath = './config/base.yaml',
+    envConfigPath = './config/development.yaml'
   }) {
-    this.configDir = configDir;
-    this.configFilesExtension = configFilesExtension;
-    this.env = env;
+    this.CLIArgs =  cliArgs(process.argv.slice(2));
+    this.baseConfigPath = this.CLIArgs.baseConfig || baseConfigPath;
+    this.envConfigPath = this.CLIArgs.envConfig || envConfigPath;
 
     this.setConfig();
   }
 
   setConfig() {
-    const envConfig = this.convertFileContentToJSON(`${this.env}.${this.configFilesExtension}`);
-    const baseConfig = this.convertFileContentToJSON(`base.${this.configFilesExtension}`);
+    const envConfig = this.convertFileContentToJSON(this.envConfigPath);
+    const baseConfig = this.convertFileContentToJSON(this.baseConfigPath);
     const CLIArgs =  cliArgs(process.argv.slice(2));
 
     const notEnvConfigs = merge(baseConfig, envConfig, CLIArgs);
@@ -28,11 +27,16 @@ module.exports = class ConfigurationManager {
     this.configuration = merge(notEnvConfigs, envConfigs);
   }
 
-  convertFileContentToJSON(fileName) {
+  convertFileContentToJSON(configPath) {
     try {
-      return yamljs.load(path.resolve(this.configDir, fileName));
+      console.log(path.extname(configPath).toLowerCase())
+      if (path.extname(configPath).toLowerCase() === '.yaml') {
+        return yamljs.load(configPath);
+      }
+      //Try to load as JSON
+      return require(path.resolve(process.env.NODE_PATH, configPath));
     } catch (e) {
-      console.log(`Config file ${fileName} can't be read.`, e);
+      console.log(`Config file ${configPath} can't be read.`, e);
       return {};
     }
   }
